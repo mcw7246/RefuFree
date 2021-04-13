b2f4055543bac589c5b475fd5ad44d07db939f23
mcw7246 <mcw7246@g.rit.edu>
2021-04-12 21:11:35 -0400

    authentication works now, i cry

--- assets/js/authentication.js
+++ assets/js/authentication.js
@@ -1,16 +1,13 @@
 const users = {"local" : "password", "refugee": "password"}
-var logged_in = false;
+
 function authentication(){
     var username = document.getElementById("username").value;
     var password = document.getElementById("password").value;
 
     if ((users[username] != undefined) && (password == users[username] )){
         var url = location.href;
-        var newUrl = url.replace("/login.html", "/homepage.html");
-        logged_in = true;
-        console.log(newUrl)
-        return window.location.replace(newUrl);
-        
+        var newUrl = url.replace("/login.html?", "/homepage.html");
+        window.location.href = newUrl;
     }
     else{
         console.log("Login unsuccessful.");
