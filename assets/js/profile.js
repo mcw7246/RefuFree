de7b91f6834cd1f28115395e7cb26c7ed94e5634
mcw7246 <mcw7246@g.rit.edu>
2021-04-14 00:23:23 -0400

    profile functionality

--- assets/js/profile.js
+++ assets/js/profile.js
@@ -3,6 +3,16 @@ function changeMode(){
     var mode = button.innerText;
     if(mode == "Edit"){
         button.innerText = "Save";
-        
+        var textboxes = document.getElementsByClassName("userInput");
+        for(var i = 0; i < textboxes.length; i++){
+            textboxes[i].readOnly = false;
+        }
+    }
+    else{
+        button.innerText = "Edit";
+        var textboxes = document.getElementsByClassName("userInput");
+        for(var i = 0; i < textboxes.length; i++){
+            textboxes[i].setAttribute("readOnly", "true");
+        }
     }
 }
